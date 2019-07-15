07d70b72e65d670b1a098cb4a03d7a3fa9508888
Pedro Coelho <pedro-luiz.coelho-rodrigues@gipsa-lab.fr>
2019-07-15 16:21:28 +0200

    ...

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='riemann_lab',
-      version='0.0dev',
-      author='Pedro Rodrigues',
-      author_email='pedro.rodrigues01@gmail.com',
-      packages=['riemann_lab']      
-      )
